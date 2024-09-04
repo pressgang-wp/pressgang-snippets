6ae6370d85ebf527aa798bfba605e43441c18712
Benedict <ben@benedict-wallis.com>
2024-09-04 15:00:32 +0100

    Check if wp.data is available before running the script.

--- src/Snippets/AcfBlockEditorColorSync.php
+++ src/Snippets/AcfBlockEditorColorSync.php
@@ -32,25 +32,28 @@ class AcfBlockEditorColorSync implements SnippetInterface {
 		$custom_colors = json_encode( $this->custom_colors ); ?>
         <script type="text/javascript">
             (function () {
-                acf.add_filter('color_picker_args', function (args) {
-                    // Get Block Editor colors from block editor settings.
-                    var block_editor_colors = wp.data.select('core/block-editor').getSettings().colors || [];
+                // Check if wp.data is available before running the script.
+                if (typeof wp !== 'undefined' && typeof wp.data !== 'undefined') {
+                    acf.add_filter('color_picker_args', function (args) {
+                        // Get Block Editor colors from block editor settings.
+                        var block_editor_colors = wp.data.select('core/block-editor').getSettings().colors || [];
 
-                    // Map Block Editor colors to an array of color values.
-                    var palette_colors = block_editor_colors.map(function (color) {
-                        return color.color;
+                        // Map Block Editor colors to an array of color values.
+                        var palette_colors = block_editor_colors.map(function (color) {
+                            return color.color;
+                        });
+
+                        // Add custom colors passed from PHP (from $args).
+                        var custom_colors = <?php echo $custom_colors; ?>;
+                        if (custom_colors.length > 0) {
+                            palette_colors = palette_colors.concat(custom_colors);
+                        }
+
+                        // Set the color palette for the ACF color picker.
+                        args.palettes = palette_colors;
+                        return args;
                     });
-
-                    // Add custom colors passed from PHP (from $args).
-                    var custom_colors = <?php echo $custom_colors; ?>;
-                    if (custom_colors.length > 0) {
-                        palette_colors = palette_colors.concat(custom_colors);
-                    }
-
-                    // Set the color palette for the ACF color picker.
-                    args.palettes = palette_colors;
-                    return args;
-                });
+                }
             })();
         </script>
 	<?php }
